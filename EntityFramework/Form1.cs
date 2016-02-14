f3bd88de0f0e926ae3847e4a40583592be1f4687
hmahdi <s.m.m.habibi@gmail.com>
2016-02-14 12:33:33 +0330

    sd

---
+++ EntityFramework/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFramework
+{
+    public partial class Form1 : Form
+    {
+        private ForeignPurchasingEntities en=new ForeignPurchasingEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            dataGridView1.DataSource = en.PayKind.ToList();
+        }
+    }
+}
